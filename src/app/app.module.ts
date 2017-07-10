56fb272d9618e26062d38882cc18571228a90163
Murillo de Morais <murillo.morais@vianuvem.com.br>
2017-07-10 14:14:38 -0300

    feat: added BookListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { RouterModule } from '@angular/router';
+import { BookListComponent } from "app/books/book-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/books/book-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-books',
+    templateUrl: 'book-list.component.html'
+})
+export class BookListComponent{
+
+}
