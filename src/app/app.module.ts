a7de336d6b390d4fa5dbabe5c4952e2e3b2b9af9
Murillo de Morais <murillo.morais@vianuvem.com.br>
2017-07-10 13:09:03 -0300

    feat: added routes control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      // { path: 'authors', component: AuthorListComponent },
+      // { path: 'author/:id', component: AuthorDetailComponent },
+      { path: 'books', component: BookListComponent },
+      // { path: 'book/:id', component: BookDetailComponent },
+      // { path: 'categories', component: CategoryListComponent },
+      // { path: 'category/:id', component: CategoryDetailComponent },
+      { path: '', redirectTo: 'books', pathMatch: 'full' },
+      // { path: '**', component: PageNotFoundComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
